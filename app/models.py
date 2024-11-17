74dc55a3acc248ee1bb1a0d28c8b3c59b29831f4
Alanna Mc <Alanna-Mc@users.noreply.github.com>
2024-11-17 18:40:50 +0000

    added password hashing method

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone
+from werkzeug.security import generate_password_hash, check_password_hash
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -41,6 +42,12 @@ class User(db.Model):
             str: A formatted string containing the username.
         """
         return f'<User {self.username}>'
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Role(db.Model):
