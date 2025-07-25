8f3f7a9ecf652243d607d85dc127ea25f1caf352
Alanna Mc <Alanna-Mc@users.noreply.github.com>
2025-07-25 02:15:11 +0100

    update port and debug

--- visionHub.py
+++ visionHub.py
@@ -23,4 +23,5 @@ def make_shell_context():
         }
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8081", debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
